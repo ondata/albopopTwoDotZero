745110fa637f4184c848d3613933c9cb2d2048f6
aborruso <aborruso@gmail.com>
2018-12-13 00:13:48 +0100

    close #34

--- script/extractGeoData.sh
+++ script/extractGeoData.sh
@@ -5,26 +5,29 @@
 # yq https://github.com/kislyuk/yq
 ### requisiti ###
 
+cartella="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-rm ./geodata.json
+rm "$cartella"/geodata.json
+rm "$cartella"/geodata.csv
 
 set -x
 
-for i in $(ls ../content/altrepa/ | egrep -i '^[a-zA-Z].+md'); do \
+for i in $(ls "$cartella"/../content/altrepa/ | egrep -i '^[a-zA-Z].+md'); do \
 tempfile="${i##*/}"
 nome="${tempfile%.*}"
 #yq . ./altrepa/"$i" ; 
 echo "$nome";
-yq '{latitude:.lat,longitude:.lng,title:.title,ipa:.ipa,nome:"'"$nome"'",tipo:"altrepa"}' ../content/altrepa/"$nome".md >>./geodata.json
+yq '{latitude:.lat,longitude:.lng,title:.title,ipa:.ipa,nome:"'"$nome"'",tipo:"altrepa"}' "$cartella"/../content/altrepa/"$nome".md >>"$cartella"/geodata.json
 done
 
 
-for i in $(ls ../content/comune/ | egrep -i '^[a-zA-Z].+md'); do \
+for i in $(ls "$cartella"/../content/comune/ | egrep -i '^[a-zA-Z].+md'); do \
 tempfile="${i##*/}"
 nome="${tempfile%.*}"
 #yq . ./altrepa/"$i" ; 
 echo "$nome";
-yq '{latitude:.lat,longitude:.lng,title:.title,ipa:.ipa,nome:"'"$nome"'",tipo:"comune"}' ../content/comune/"$nome".md >>./geodata.json
+yq '{latitude:.lat,longitude:.lng,title:.title,ipa:.ipa,nome:"'"$nome"'",tipo:"comune"}' "$cartella"/../content/comune/"$nome".md >>"$cartella"/geodata.json
 done
 
-mlr --j2c cat ./geodata.json >./geodata.csv
+mlr --j2c cat "$cartella"/geodata.json >"$cartella"/data.csv
+mlr --csv filter '$latitude > 1' "$cartella"/data.csv >"$cartella"/geodata.csv
